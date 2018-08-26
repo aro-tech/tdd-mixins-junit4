f73b1dd95c7dc99aad96d67e4c309ef2d25c33d8
aro_tech <aro_tech@gmx.com>
2018-08-26 07:30:40 +0200

    Fix breaking change in assertj by renaming junit fail method to junitFail

--- src/main/java/com/github/aro_tech/tdd_mixins/AllAssertions.java
+++ src/main/java/com/github/aro_tech/tdd_mixins/AllAssertions.java
@@ -10,19 +10,11 @@ package com.github.aro_tech.tdd_mixins;
 public interface AllAssertions extends AssertJ, JUnitAssert {
 	
 	/**
-	 * Call org.junit.Assert.fail() 
-	 * Resolves conflict between JUnit and AssertJ in favor of JUnit
-	 */
-	@Override
-	default void fail(String message) {
-		JUnitAssert.super.fail(message);
-	}
-	
-	/**
 	 * Call AssertJ.fail()
 	 * @param message
 	 */
 	default void assertJFail(String message) {
 		AssertJ.super.fail(message);
 	}
+	
 }
